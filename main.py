cb28601be40d5f6261e2d3e4655529486ef28247
Arthur Goetzke-Coburn <arthur.goetzkecoburn@gmail.com>
2023-03-26 23:38:22 -0400

    added motor class, added FSR code

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
 import machine
-from machine import Pin
+from machine import ADC, Pin
 
 
 class Motor:
-    def __init__(self, mid, dir1, dir2, pwm_pin, hA, hB):
+    def __init__(self, mid, dir1, dir2, pwm_pin):
         self.id = mid
         self.dir1 = dir1
         self.dir2 = dir2
-        self.hA = hA
-        self.hB = hB
         self.pos = 0
         self.target_pos = 0
         self.dir = MOTOR_OFF
@@ -16,17 +14,36 @@ class Motor:
         self.pwm = machine.PWM(pwm_pin)
         self.pwm.freq(500)
         self.pwm.duty_u16(self.speed)
+        self.state = 0
 
-    def move_to(self):
-        if abs(self.pos - self.target_pos) > 10:
-            if (self.pos - self.target_pos) > 0:
-                self.dir = MOTOR_CW
-            else:
-                self.dir = MOTOR_CCW
+    def move_to(self, target):
+        self.target_pos = target
+        if (self.pos - self.target_pos) > 0:
+            self.dir = MOTOR_CW
         else:
-            self.dir = MOTOR_OFF
+            self.dir = MOTOR_CCW
+        self.state = 2
+
+    def move(self, direction):
+        self.dir = direction
+        self.state = 1
+
+    def stop(self):
+        self.dir = MOTOR_OFF
+        self.state = 0
+
+    def update_pos(self, cnts):
+        self.pos = cnts
+
+    def update(self, cnts):
+        self.update_pos(cnts)
+        if self.state == 2:
+            if abs(self.pos - self.target_pos) < 10:
+                self.stop()
 
 
+resistorPin = Pin(17, Pin.IN, Pin.PULL_DOWN)
+fsrADC = ADC(resistorPin)
 
 
 hallMAid = 5
@@ -67,6 +84,14 @@ MOTOR_CCW = [0, 1]
 MOTOR_OFF = [0, 0]
 motorMdir = MOTOR_OFF
 motorSdir = MOTOR_OFF
-
+POS_START = 0
+motorM = Motor(0, 10, 11, 12)
+motorS = Motor(0, 13, 14, 15)
+FSR_THRESHOLD = (0.8*65535)
 while True:
-    pass
+    force = fsrADC.read_u16()
+    if force > FSR_THRESHOLD:
+        motorM.stop()
+        motorS.stop()
+    motorM.update(cntM)
+    motorS.update(cntS)
