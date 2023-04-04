8044eab85fc8cbdfbd9623126b446ad0404e42e8
Arthur Goetzke-Coburn <arthur.goetzkecoburn@gmail.com>
2023-04-03 20:15:40 -0400

    added stepper motor driver (A4988) class

--- main.py
+++ main.py
@@ -1,8 +1,49 @@
 import machine
+import time
 from machine import ADC, Pin
 
 
-class Motor:
+class A4988Stepper:
+    def __init__(self, mid, step_pin, dir_pin, en_pin, steps_per_rev):
+        self.id = mid
+        self.step_pin = Pin(step_pin, Pin.OUT)
+        self.dir_pin = Pin(dir_pin, Pin.OUT)
+        self.en_pin = Pin(en_pin, Pin.OUT)
+        self.dir = 0
+        self.steps_per_rev = steps_per_rev
+        self.moving = False
+        self.speed = 0
+        self.time_between_steps = 0
+        self.time_next_step = time.ticks_ms()
+        self.enable()
+
+    def enable(self):
+        self.en_pin.value(1)
+
+    def disable(self):
+        self.en_pin.value(0)
+
+    def stop(self):
+        self.moving = False
+
+    def move(self, dir, speed):
+        self.dir = dir
+        self.dir_pin.value(dir)
+        self.speed = speed
+        self.time_between_steps = 1000 * self.speed * self.steps_per_rev / 60
+        self.moving = True
+        self.time_next_step = time.ticks_ms()
+
+    def update(self):
+        if self.moving:
+            if time.ticks_diff(time.ticks_ms(), self.time_next_step) > 0:
+                self.step_pin.value(1)
+                time.sleep_us(3)
+                self.step_pin.value(0)
+                self.time_next_step = time.ticks_add(time.ticks_ms(), self.time_between_steps)
+
+
+class DCMotor:
     def __init__(self, mid, dir1, dir2, pwm_pin):
         self.id = mid
         self.dir1 = Pin(dir1, Pin.OUT)
@@ -86,8 +127,8 @@ MOTOR_OFF = [0, 0]
 motorMdir = MOTOR_OFF
 motorSdir = MOTOR_OFF
 POS_START = 0
-motorM = Motor(0, 10, 11, 12)
-motorS = Motor(1, 13, 14, 16)
+motorM = DCMotor(0, 10, 11, 12)
+motorS = DCMotor(1, 13, 14, 16)
 FSR_THRESHOLD = (0.8 * 65535)
 motorM.move_to(800)
 motorS.move_to(800)
